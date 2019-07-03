4492ae60d8ada27dfc4380cf23d3bcdedee8c080
Vardex <12969608+Vardex@users.noreply.github.com>
2019-07-03 18:41:47 +0200

    Add check for tooltip text

--- Translit.lua
+++ Translit.lua
@@ -65,7 +65,9 @@ end
 local function Tooltip(self)
   for i=1, self:NumLines() do
     local text = _G["GameTooltipTextLeft"..i]
-    text:SetText(((LibTranslit:Transliterate(text:GetText(), options.mark) or "")))
+    if text then
+      text:SetText(((LibTranslit:Transliterate(text:GetText(), options.mark) or "")))
+    end
   end
 end
 
